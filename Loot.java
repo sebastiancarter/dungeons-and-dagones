da12a054cfe5b93e0688a626fb579011663278ae
NateMilbank <37712509+NateMilbank@users.noreply.github.com>
2018-03-23 14:02:15 -0600

    Add files via upload

---
+++ Loot.java
@@ -0,0 +1,17 @@
+
+public class Loot {
+/* Percentage chance for loot
+ * Hp pot: 20%
+ * Res pot: 30%
+ * Sword: 20%
+ * Armor: 20%
+ * 
+ * Hp pot % Rand0_4s1(){num=next.Int(5); return(num); }; if(num==0) get Hp pot
+ * Res pot % Rand0_9s1(){num=next.Int(10); return(num); }; if(num==0||num==1||num=2) get res pot
+ * Sword % Rand0_4s1(){num=next.Int(5); return(num); }; if(num==0) get new sword
+ * Armor % Rand0_4s1(){num=next.Int(5); return(num); }; if(num==0) get new armor
+ * 
+ * Sword base damage 10-40  rand10_40s1(){num=next.Int(31)+10; return(num);
+ * Armor health increase ?
+ */
+}
